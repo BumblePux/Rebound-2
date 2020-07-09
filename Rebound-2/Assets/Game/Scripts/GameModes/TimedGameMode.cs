eb2fa252ba93d8aa55d3b387617d7f261da9f372
Adam <bumblepux@gmail.com>
2020-07-09 23:18:24 +0100

    Created TimedGameMode class. Base skeleton code added from previous prototype.

---
+++ Rebound-2/Assets/Game/Scripts/GameModes/TimedGameMode.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BumblePux.Rebound.GameModes
+{
+    public class TimedGameMode : GameModeBase
+    {
+        [Header("Timed Mode Base Settings")]
+        public float CoyoteTime = 0.2f;
+
+        [Header("Countdown Timer Settings")]
+        public float MaxTime = 30f;
+        public float TimeBonus = 2f;
+        public float TimePenalty = 4f;
+
+        [Header("Player Speed Settings")]
+        public float StartSpeed = 50f;
+        public float MaxSpeed = 400f;
+        public float SpeedIncrementOnTargetHit = 5f;
+
+        [Header("Planet Enable Settings")]
+        public int ScoreToEnableSecondPlanet = 15;
+        public int ScoreToEnableThirdPlanet = 40;
+
+        private float currentTime;
+        private float currentPlayerSpeed;
+        private int currentScore;
+
+        private bool secondPlanetEnabled;
+        private bool thirdPlanetEnabled;
+
+
+
+        private void Start()
+        {
+            StartGameLoop();
+        }
+
+        protected override IEnumerator GameInitialize()
+        {
+            Debug.Log("Game Initialize");
+
+            yield return null;
+        }
+
+        protected override IEnumerator GameInProgress()
+        {
+            Debug.Log("Game in progress");
+
+            yield return null;
+        }
+
+        protected override IEnumerator GameOver()
+        {
+            Debug.Log("Game Over");
+
+            yield return null;
+        }
+
+        public override void TargetHit()
+        {
+            Debug.Log("Target hit");
+        }
+
+        public override void TargetMissed()
+        {
+            Debug.Log("Target missed");
+        }
+    }
+}
--- Rebound-2/Assets/Game/Scripts/Managers/GameManager.cs
+++ Rebound-2/Assets/Game/Scripts/Managers/GameManager.cs
@@ -6,6 +6,41 @@ namespace BumblePux.Rebound.Managers
 {
     public class GameManager : Singleton<GameManager>
     {
+        [Header("Debug")]
+        [SerializeField] bool FindGameModeOnStart;
+
         public GameModeBase CurrentGameMode { get; set; }
+
+
+        private void Start()
+        {
+            RunDebugMethods();
+        }
+
+
+#if UNITY_EDITOR
+
+        private void RunDebugMethods()
+        {
+
+        }
+
+        private void TryFindGameModeOnStart()
+        {
+            if (FindGameModeOnStart)
+            {
+                CurrentGameMode = FindObjectOfType<GameModeBase>();
+                if (CurrentGameMode)
+                {
+                    Debug.Log($"{CurrentGameMode.GetType().Name} GameMode found!");
+                }
+                else
+                {
+                    Debug.Log("No GameMode found.");
+                }
+            }
+        }
+
+#endif
     }
 }
