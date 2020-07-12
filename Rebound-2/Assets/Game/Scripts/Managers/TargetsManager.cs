62d270e372aa9ca0478022ede49844686259cf9f
Adam <bumblepux@gmail.com>
2020-07-12 16:45:10 +0100

    Created TargetsManager.cs
    TargetsManager is called by the GameMode, and it instantiates and sets up a Target for each planet.

---
+++ Rebound-2/Assets/Game/Scripts/Managers/TargetsManager.cs
@@ -0,0 +1,24 @@
+﻿using BumblePux.Rebound.Interactables;
+using BumblePux.Tools.Singleton;
+using UnityEngine;
+
+namespace BumblePux.Rebound.Managers
+{
+    public class TargetsManager : Singleton<TargetsManager>
+    {
+        [Header("Target Settings")]
+        public Target TargetPrefab;
+
+
+        public void Initialize()
+        {
+            var planets = PlanetsManager.Instance.GetAllPlanets();
+
+            for (int i = 0; i < planets.Length; i++)
+            {
+                var target = Instantiate(TargetPrefab, Vector3.zero, Quaternion.identity, planets[i]);
+                target.Initialize();
+            }
+        }
+    }
+}
