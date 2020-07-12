53f7c3a0a50b5d934d9916faa7a0f92cc533e0b1
Adam <bumblepux@gmail.com>
2020-07-12 19:39:42 +0100

    Created PlayerInteraction.cs. Code copied from previous prototype.

---
+++ Rebound-2/Assets/Game/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,49 @@
+﻿using BumblePux.Rebound.GameModes;
+using BumblePux.Rebound.Interactables;
+using BumblePux.Rebound.Managers;
+using UnityEngine;
+
+namespace BumblePux.Rebound.Player
+{
+    public class PlayerInteraction : MonoBehaviour
+    {
+        [Header("Debug Settings")]
+        public bool AutoInteract;
+
+        private IInteractable interactable;
+        private GameModeBase gameMode;
+
+
+        private void Awake()
+        {
+            gameMode = GameManager.Instance.CurrentGameMode;
+        }
+
+        public void TryInteract()
+        {
+            if (interactable != null)
+            {
+                interactable.Interact();
+            }
+            else
+            {
+                gameMode.TargetMissed();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            interactable = other.GetComponentInParent<IInteractable>();
+
+            if (interactable != null && AutoInteract)
+            {
+                TryInteract();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D pther)
+        {
+            interactable = null;
+        }
+    }
+}
