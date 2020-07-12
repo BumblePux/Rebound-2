3a61617378cd909613cf153c193d28c46a3bbea2
Adam <bumblepux@gmail.com>
2020-07-12 23:39:42 +0100

    Created Timed game mode UI class

---
+++ Rebound-2/Assets/Game/Scripts/UI/TimedGameModeUI.cs
@@ -0,0 +1,47 @@
+﻿using BumblePux.Rebound.GameModes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BumblePux.Rebound.UI
+{
+    public class TimedGameModeUI : MonoBehaviour
+    {
+        [Header("Game Mode")]
+        public TimedGameMode timedMode;
+
+        [Header("Time UI Elements")]
+        public TMP_Text TimeLabel;
+        public Image TimeCircle;
+
+        [Header("Score UI Elements")]
+        public TMP_Text ScoreLabel;
+
+
+
+        private void OnEnable()
+        {
+            timedMode.OnTimeChanged += UpdateTime;
+            timedMode.OnScoreChanged += UpdateScore;
+        }
+
+        private void OnDisable()
+        {
+            timedMode.OnTimeChanged -= UpdateTime;
+            timedMode.OnScoreChanged -= UpdateScore;
+        }
+
+        public void UpdateTime(float currentTime, float maxTime)
+        {
+            TimeLabel.SetText(currentTime.ToString("F0") + "s");
+
+            float percent = currentTime / maxTime;
+            TimeCircle.fillAmount = percent;
+        }
+
+        public void UpdateScore(int currentScore)
+        {
+            ScoreLabel.SetText(currentScore.ToString());
+        }
+    }
+}
