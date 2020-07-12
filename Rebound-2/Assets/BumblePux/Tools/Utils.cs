4b4b7bd15d88247571793d0c551dc4ecee5652f0
Adam <bumblepux@gmail.com>
2020-07-12 14:39:02 +0100

    Created static Utils class with a CoinToss method.
    Utils is a static class located under the BumblePux.Tools namespace.

---
+++ Rebound-2/Assets/BumblePux/Tools/Utils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace BumblePux.Tools
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Will randomly return true or false. Simulating the flip of a coin.
+        /// </summary>
+        /// <returns></returns>
+        public static bool CoinToss()
+        {
+            int flip = Random.Range(0, 2);
+            if (flip == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
