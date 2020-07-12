b473f268912b4da02677db9a3726c2386beb1d5e
Adam <bumblepux@gmail.com>
2020-07-12 23:39:50 +0100

    Update TimedGameMode.cs

--- Rebound-2/Assets/Game/Scripts/GameModes/TimedGameMode.cs
+++ Rebound-2/Assets/Game/Scripts/GameModes/TimedGameMode.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using BumblePux.Rebound.Interactables;
+using BumblePux.Rebound.Managers;
+using BumblePux.Rebound.Player;
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace BumblePux.Rebound.GameModes
 {
     public class TimedGameMode : GameModeBase
     {
+        public event Action<float, float> OnTimeChanged;
+        public event Action<int> OnScoreChanged;
+
+        [Header("Required Prefabs")]
+        public PlayerMovement PlayerPrefab;
+
         [Header("Timed Mode Base Settings")]
         public float CoyoteTime = 0.2f;
 
@@ -23,22 +33,58 @@ namespace BumblePux.Rebound.GameModes
         public int ScoreToEnableThirdPlanet = 40;
 
         private float currentTime;
-        private float currentPlayerSpeed;
+        public float CurrentTime
+        {
+            get => currentTime;
+            set
+            {
+                currentTime = value;
+                OnTimeChanged?.Invoke(currentTime, MaxTime);
+            }
+        }
+
         private int currentScore;
+        public int CurrentScore
+        {
+            get => currentScore;
+            set
+            {
+                currentScore = value;
+                OnScoreChanged?.Invoke(currentScore);
+            }
+        }
+
+        private float currentPlayerSpeed;
 
         private bool secondPlanetEnabled;
         private bool thirdPlanetEnabled;
+
+        private PlayerMovement player;
 
 
 
         private void Start()
         {
+            PlanetsManager.Instance.Initialize();
+            TargetsManager.Instance.Initialize();
+            player = Instantiate(PlayerPrefab, Vector3.zero, Quaternion.identity);
+
+            currentPlayerSpeed = StartSpeed;
+
             StartGameLoop();
         }
 
         protected override IEnumerator GameInitialize()
         {
             Debug.Log("Game Initialize");
+
+            IsGameOver = false;
+            HasGameStarted = false;
+
+            CurrentTime = MaxTime;
+            player.Speed = currentPlayerSpeed;
+
+            player.Initialize();
 
             yield return null;
         }
@@ -47,24 +93,68 @@ namespace BumblePux.Rebound.GameModes
         {
             Debug.Log("Game in progress");
 
-            yield return null;
+            HasGameStarted = true;
+
+            while (!IsGameOver)
+            {
+                CurrentTime -= Time.deltaTime;
+
+                if (CurrentTime <= float.Epsilon)
+                {
+                    CurrentTime = 0f;
+                    IsGameOver = true;
+                }
+
+                yield return null;
+            }            
         }
 
         protected override IEnumerator GameOver()
         {
             Debug.Log("Game Over");
 
+            // Show game over UI
+
             yield return null;
         }
 
-        public override void TargetHit()
+        public override void TargetHit(Target target)
         {
-            Debug.Log("Target hit");
+            player.TryChangeDirection(true);
+
+            CurrentScore++;
+
+            CurrentTime += TimeBonus;
+            if (CurrentTime > MaxTime)
+                CurrentTime = MaxTime;
+            
+            if (currentPlayerSpeed < MaxSpeed)
+            {
+                currentPlayerSpeed += SpeedIncrementOnTargetHit;
+                if (currentPlayerSpeed > MaxSpeed)
+                    currentPlayerSpeed = MaxSpeed;
+
+                player.Speed = currentPlayerSpeed;
+            }
+
+            if (CurrentScore >= ScoreToEnableSecondPlanet && !secondPlanetEnabled)
+            {
+                PlanetsManager.Instance.PlanetCount++;
+                secondPlanetEnabled = true;
+            }
+
+            if (CurrentScore >= ScoreToEnableThirdPlanet && !thirdPlanetEnabled)
+            {
+                PlanetsManager.Instance.PlanetCount++;
+                thirdPlanetEnabled = true;
+            }
         }
 
         public override void TargetMissed()
         {
             Debug.Log("Target missed");
+
+            CurrentTime -= TimePenalty;
         }
     }
 }
