5ddca4bbc5f4448e7efb0afbbe6ed9edda5e1124
Adam <bumblepux@gmail.com>
2020-07-14 18:35:37 +0100

    Created GameplayStatics.cs. Class contains static helper methods

---
+++ Rebound-2/Assets/Game/Scripts/Helpers/GameplayStatics.cs
@@ -0,0 +1,13 @@
+﻿using BumblePux.Rebound.GameModes;
+using BumblePux.Rebound.Managers;
+
+namespace BumblePux.Rebound
+{
+    public static class GameplayStatics
+    {
+        public static GameModeBase GetGameMode()
+        {
+            return GameManager.Instance.CurrentGameMode;
+        }
+    }
+}
