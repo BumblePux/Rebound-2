c8cc2c66cd68ec45be51da344a6913243b0eebaa
Adam <bumblepux@gmail.com>
2020-07-14 22:06:57 +0100

    Created MainMenu.cs

---
+++ Rebound-2/Assets/Game/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BumblePux.Rebound.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            GameplayStatics.GetGameManager().CurrentGameMode = null;
+        }
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("TimedGameMode");
+        }
+
+        public void LoadLeaderboards()
+        {
+            Debug.Log("Would load leaderboards UI.");
+        }
+
+        public void LoadAchievements()
+        {
+            Debug.Log("Would load achievements UI.");
+        }
+    }
+}
