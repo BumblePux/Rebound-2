19fda4a4c97cf8083fe6899f2eb395f3984eea63
Adam <bumblepux@gmail.com>
2020-07-21 18:34:52 +0100

    Created Unlockable scriptableobject

---
+++ Rebound-2/Assets/Game/Scripts/Unlockables/Unlockable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BumblePux.Rebound.Unlockables
+{
+    [CreateAssetMenu(fileName = "New Unlockable", menuName = "Unlockables/Unlockable")]
+    public class Unlockable : ScriptableObject
+    {
+        public string Name;
+        public GameObject Prefab;
+        public Sprite PreviewImage;
+        public bool IsUnlocked;
+    }
+}
