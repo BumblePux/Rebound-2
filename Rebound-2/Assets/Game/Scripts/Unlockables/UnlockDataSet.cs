0667d4da7629be2e24472a9d608a94b9ea8aea36
Adam <bumblepux@gmail.com>
2020-07-21 18:35:25 +0100

    Created UnlockDataset.cs scriptableobject

---
+++ Rebound-2/Assets/Game/Scripts/Unlockables/UnlockDataSet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BumblePux.Rebound.Unlockables
+{
+    [CreateAssetMenu(fileName = "New Unlock Dataset", menuName = "Unlockables/New Dataset")]
+    public class UnlockDataSet : ScriptableObject
+    {
+        public Unlockable[] Unlockables;
+    }
+}
