0da8042313491dfe9b7dc753a06575118f007f64
Adam <bumblepux@gmail.com>
2020-08-02 21:07:55 +0100

    Copied GameManager.cs to code to GameInstance.cs

--- Rebound-2/Assets/Game/Scripts/GameInstance.cs
+++ Rebound-2/Assets/Game/Scripts/GameInstance.cs
@@ -1,13 +1,30 @@
 ﻿using BumblePux.Rebound.GameModes;
+using BumblePux.Rebound.Unlockables;
 using BumblePux.Tools.Singleton;
 using UnityEngine;
 
 namespace BumblePux
 {
+    [DefaultExecutionOrder(-100)]
     public class GameInstance : Singleton<GameInstance>
     {
         [Header("Current References")]
         [SerializeField] private GameModeBase currentGameMode = default;
         public GameModeBase CurrentGameMode { get => currentGameMode; set => currentGameMode = value; }
+
+        [Header("Data Sets")]
+        [SerializeField] UnlockDataSet ShipDataSet = default;
+
+        public Unlockable SelectedShip { get; set; }
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            currentGameMode = null;
+
+            SelectedShip = ShipDataSet.Unlockables[0];
+        }
     }
 }
