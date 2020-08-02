be41a6acec85238ff01208f12b33cb4c17062430
Adam <bumblepux@gmail.com>
2020-08-02 20:50:09 +0100

    Created HUD.cs base class
    HUD is designed to be super class for all UI based classes, such as a main menu or the display for a game mode.

---
+++ Rebound-2/Assets/Game/Scripts/UI/HUD.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BumblePux.Rebound.UI
+{
+    public abstract class HUD : Actor
+    {
+        public abstract void Show();
+        public abstract void Hide();
+    }
+}
