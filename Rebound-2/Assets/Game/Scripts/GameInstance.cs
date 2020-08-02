3eb8b9da642b64d94de90cfff29f68058641c58a
Adam <bumblepux@gmail.com>
2020-08-02 18:01:47 +0100

    Created GameInstance.cs. This will replace GameManager.cs

---
+++ Rebound-2/Assets/Game/Scripts/GameInstance.cs
@@ -0,0 +1,13 @@
+﻿using BumblePux.Rebound.GameModes;
+using BumblePux.Tools.Singleton;
+using UnityEngine;
+
+namespace BumblePux
+{
+    public class GameInstance : Singleton<GameInstance>
+    {
+        [Header("Current References")]
+        [SerializeField] private GameModeBase currentGameMode = default;
+        public GameModeBase CurrentGameMode { get => currentGameMode; set => currentGameMode = value; }
+    }
+}
