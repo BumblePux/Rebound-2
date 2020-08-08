ff169a8e91dd68e3ee82bd937e3bc94cd7a33071
Adam <bumblepux@gmail.com>
2020-08-08 21:50:52 +0100

    Created BackgroundScroll.cs to allow for moving star effect for BackgroundStars material

---
+++ Rebound-2/Assets/Game/Scripts/Effects/BackgroundScroll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BumblePux.Rebound.Effects
+{
+    public class BackgroundScroll : MonoBehaviour
+    {
+        [SerializeField] Image ScrollImage = default;
+        [SerializeField] Vector2 OffsetPerSecond = default;
+        [SerializeField] int MaxOffsetToReset = 1;
+
+        private void Update()
+        {
+            var offset = ScrollImage.material.GetTextureOffset("_MainTex");
+
+            offset += OffsetPerSecond * Time.deltaTime;
+            ScrollImage.material.SetTextureOffset("_MainTex", new Vector2(offset.x, offset.y));
+
+            var tilingX = Mathf.Abs(offset.x);
+            var tilingY = Mathf.Abs(offset.y);
+
+
+            if (tilingX >= MaxOffsetToReset || tilingY >= MaxOffsetToReset)
+            {
+                ScrollImage.material.SetTextureOffset("_MainTex", Vector2.zero);
+            }
+        }
+
+        private void OnDisable()
+        {
+            ScrollImage.material.SetTextureOffset("_MainTex", Vector2.zero);
+        }
+    }
+}
