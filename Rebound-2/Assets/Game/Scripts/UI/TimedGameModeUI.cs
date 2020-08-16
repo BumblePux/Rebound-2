ed8c516cdd2858f8e03c61d31a111cf3bbfe4f3d
Adam <bumblepux@gmail.com>
2020-08-16 16:38:28 +0100

    TimedGameModeUI now gets updated by TimedGameMode

--- Rebound-2/Assets/Game/Scripts/UI/TimedGameModeUI.cs
+++ Rebound-2/Assets/Game/Scripts/UI/TimedGameModeUI.cs
@@ -18,40 +18,28 @@ namespace BumblePux.Rebound.UI
         [Header("Score UI Elements")]
         public TMP_Text ScoreLabel;
 
-        private TimedGameMode timedMode;
-
-        float lastTime = -1f;
-        int lastScore = -1;
-
-        private void Awake()
-        {
-            timedMode = GetGameMode() as TimedGameMode;
-        }
-
-        private void Update()
-        {
-            if (lastTime != timedMode.CurrentTime)
-                UpdateTime(timedMode.CurrentTime, timedMode.MaxTime);
-
-            if (lastScore != timedMode.CurrentScore)
-                UpdateScore(timedMode.CurrentScore);
-        }
+        
 
         public void UpdateTime(float currentTime, float maxTime)
         {
             TimeLabel.SetText(currentTime.ToString("F0") + "s");
-
-            float percent = currentTime / maxTime;
-            TimeCircle.fillAmount = percent;
-
-            lastTime = currentTime;
         }
 
         public void UpdateScore(int currentScore)
         {
             ScoreLabel.SetText(currentScore.ToString());
+        }
 
-            lastScore = currentScore;
+        public override void Show()
+        {
+            TimerGroup.SetActive(true);
+            ScoreGroup.SetActive(true);
+        }
+
+        public override void Hide()
+        {
+            TimerGroup.SetActive(false);
+            ScoreGroup.SetActive(false);
         }
     }
 }
